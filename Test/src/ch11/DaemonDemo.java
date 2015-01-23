eba8f6dd8760e18f863016435fbd1799423c16fd
unknown <tw00182691@gmail.com>
2015-01-23 14:15:12 +0800

    別說啦～

--- Test/src/ch11/DaemonDemo.java
+++ Test/src/ch11/DaemonDemo.java
@@ -1,28 +1,31 @@
 package ch11;
 
-public class DaemonDemo {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-	public static void main(String[] args) {
-		Thread thread = new Thread() {
-			public void run() {
-				while (true) {
-					System.out.println("ʨrz");
-				}
-			}
-		};
-		Thread thread_new = new Thread() {
-			public void run() {
-				while (true) {
-					System.out.println("Orz");
-				}
-			}
-		};
-		thread_new.setPriority(1);
-		thread_new.setDaemon(true);
-		thread_new.start();
-		thread.setPriority(5);
-		thread.setDaemon(true);
-		thread.start();
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.Arrays;
+
+public class DaemonDemo extends JFrame {
+	public static void main(String[] args){
+
+        System.out.println("main()方法開始執行了。");
+        // 輸出錯誤資訊
+        System.err.println("在執行期間手動輸出一個錯誤資訊：");
+        System.err.println("\t該軟件沒有買保險，請注意安全");
+        System.out.println("PrintErrorAndDebug.main()");
+        System.out.println("main()方法執行結束。");
+    
 	}
-
 }
+
